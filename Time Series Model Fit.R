2d5706af3652e18535ab85168fea6a56e332dd33
Swarnajit Podder <110167864+Swarnajit2001@users.noreply.github.com>
2023-11-04 15:25:17 +0530

    Add files via upload

---
+++ Time Series Model Fit.R
@@ -0,0 +1,512 @@
+library(forecast)
+library(ggplot2)
+library(tseries)
+library(Metrics)
+library(tseries)
+library(rugarch)
+library(FinTS)
+
+
+#1. BPCL
+#=======
+#data
+data = read.csv('BPCL.NS.csv')
+
+
+#plotting the data against time
+ggplot(data)+
+  geom_line(aes(x = 1:nrow(data), y = Close))
+
+#dividing into train and test
+div = (floor(nrow(data)))*0.8
+train_data = data[1:div,]
+test_data = data[(div+1):nrow(data),]
+
+
+
+#checking stationarity of the data
+adf.test(train_data$Close)
+
+#taking first order difference
+y = diff(train_data$Close)
+
+#again checking stationarity of the data
+adf.test(y)
+
+#plotting the stationary data
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#finding the order of AR & MA using the acf and pacf
+acf(y)
+pacf(y)
+
+#checking order with auto.arima function
+auto.arima(train_data$Close)
+
+#fitting the arima model with p,d,q
+model_train = arima(train_data$Close, order = c(0,1,0))
+y.fit = forecast(model_train, length(test_data$Close))[[4]]
+
+#RMSE
+rmse(test_data$Close, y.fit)
+
+#plots
+ggplot()+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  geom_line(data = data, aes(x = 1 : nrow(data), y = Close, col = 'Original'))+
+  geom_line(data = test_data, aes(x = (div+1):nrow(data), y = y.fit, col = 'fitted'), size = 0.75)+
+  labs(x = 'Date',
+       y = 'Closing Price',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+  
+#----------------------------------------------
+
+#ARCH-GARCH
+
+#checking volatility clustering
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#checking whether to fit ARCH model or not
+ArchTest(y)
+
+y1 = diff(data$Close)
+
+#determining the order
+garch(x = y, grad = 'numerical', trace = FALSE)
+
+#fitting GARCH model
+y_garch = ugarchspec(variance.model = list(garchOrder = c(1,1)), mean.model=list(armaOrder=c(0,0)))
+
+y_garch_fit = ugarchfit(y_garch, data = y)
+
+y_garch_fit
+
+
+#forecasting
+y2 = diff(test_data$Close)
+y_forecast = (ugarchforecast(y_garch_fit, n.ahead = length(y2))@forecast)$series
+
+
+#plot(y_garch_fit)
+
+ggplot()+
+  geom_line(aes(x = 1 : length(y1), y = y1, col = 'Original'))+
+  geom_line(aes(x = (div+1):(nrow(data)-1), y = y_forecast, col = 'Fitted'), size = 0.75)+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  labs(x = 'Date',
+       y = 'Closing Price (stationary)',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+  
+  
+
+
+rmse(y2,y_forecast)
+#====================================================================================
+
+#2. CIPLA
+#=========
+#data
+data = read.csv('CIPLA.NS.csv')
+
+
+#plotting the data against time
+ggplot(data)+
+  geom_line(aes(x = 1:nrow(data), y = Close))
+
+#dividing into train and test
+div = (floor(nrow(data)))*0.8
+train_data = data[1:div,]
+test_data = data[(div+1):nrow(data),]
+
+
+
+#checking stationarity of the data
+adf.test(train_data$Close)
+
+#taking first order difference
+y = diff(train_data$Close)
+
+#again checking stationarity of the data
+adf.test(y)
+
+#plotting the stationary data
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#finding the order of AR & MA using the acf and pacf
+acf(y)
+pacf(y)
+
+#checking order with auto.arima function
+auto.arima(train_data$Close)
+
+#fitting the arima model with p,d,q
+model_train = arima(train_data$Close, c(0,1,1))
+y.fit = forecast(model_train, length(test_data$Close))[[4]]
+
+#RMSE
+rmse(test_data$Close, y.fit)
+
+#plots
+ggplot()+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  geom_line(data = data, aes(x = 1 : nrow(data), y = Close, col = 'Original'))+
+  geom_line(data = test_data, aes(x = (div+1):nrow(data), y = y.fit, col = 'fitted'), size = 0.75)+
+  labs(x = 'Date',
+       y = 'Closing Price',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+#-----------------------------------------------------------------------------
+#ARCH-GARCH
+
+#checking volatility clustering
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#checking whether to fit ARCH model or not
+ArchTest(y)
+
+y1 = diff(data$Close)
+
+#determining the order
+garch(x = y, grad = 'numerical', trace = FALSE)
+
+#fitting GARCH model
+y_garch = ugarchspec(variance.model = list(garchOrder = c(1,1)), mean.model=list(armaOrder=c(0,1)))
+
+y_garch_fit = ugarchfit(y_garch, data = y)
+
+y_garch_fit
+
+
+#forecasting
+y2 = diff(test_data$Close)
+y_forecast = (ugarchforecast(y_garch_fit, n.ahead = length(y2))@forecast)$series
+
+
+#plot(y_garch_fit)
+
+ggplot()+
+  geom_line(aes(x = 1 : length(y1), y = y1, col = 'Original'))+
+  geom_line(aes(x = (div+1):(nrow(data)-1), y = y_forecast, col = 'Fitted'), size = 0.75)+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  labs(x = 'Date',
+       y = 'Closing Price (stationary)',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+
+
+
+
+rmse(y2,y_forecast)
+#====================================================================================
+
+#3. COROMANDEL
+#==============
+
+#data
+data = read.csv('COROMANDEL.NS.csv')
+
+
+#plotting the data against time
+ggplot(data)+
+  geom_line(aes(x = 1:nrow(data), y = Close))
+
+#dividing into train and test
+div = (floor(nrow(data)))*0.8
+train_data = data[1:div,]
+test_data = data[(div+1):nrow(data),]
+
+
+
+#checking stationarity of the data
+adf.test(train_data$Close)
+
+#taking first order difference
+y = diff(train_data$Close)
+
+#again checking stationarity of the data
+adf.test(y)
+
+#plotting the stationary data
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#finding the order of AR & MA using the acf and pacf
+acf(y)
+pacf(y)
+
+#checking order with auto.arima function
+auto.arima(train_data$Close)
+
+#fitting the arima model with p,d,q
+model_train = arima(train_data$Close, c(0,1,0))
+y.fit = forecast(model_train, length(test_data$Close))[[4]]
+
+#RMSE
+rmse(test_data$Close, y.fit)
+
+#plots
+ggplot()+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  geom_line(data = data, aes(x = 1 : nrow(data), y = Close, col = 'Original'))+
+  geom_line(data = test_data, aes(x = (div+1):nrow(data), y = y.fit, col = 'fitted'), size = 0.75)+
+  labs(x = 'Date',
+       y = 'Closing Price',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+#------------------------------------------------------------------------------------
+#ARCH-GARCH
+
+#checking volatility clustering
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#checking whether to fit ARCH model or not
+ArchTest(y)
+
+y1 = diff(data$Close)
+
+#determining the order
+garch(x = y, grad = 'numerical', trace = FALSE)
+
+#fitting GARCH model
+y_garch = ugarchspec(variance.model = list(garchOrder = c(1,1)), mean.model=list(armaOrder=c(0,0)))
+
+y_garch_fit = ugarchfit(y_garch, data = y)
+
+y_garch_fit
+
+
+#forecasting
+y2 = diff(test_data$Close)
+y_forecast = (ugarchforecast(y_garch_fit, n.ahead = length(y2))@forecast)$series
+
+
+#plot(y_garch_fit)
+
+ggplot()+
+  geom_line(aes(x = 1 : length(y1), y = y1, col = 'Original'))+
+  geom_line(aes(x = (div+1):(nrow(data)-1), y = y_forecast, col = 'Fitted'), size = 0.75)+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  labs(x = 'Date',
+       y = 'Closing Price (stationary)',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+
+
+
+
+rmse(y2,y_forecast)
+#====================================================================================
+
+#4 HDFC
+#=======
+
+#data
+data = read.csv('HDFCBANK.NS.csv')
+
+
+#plotting the data against time
+ggplot(data)+
+  geom_line(aes(x = 1:nrow(data), y = Close))
+
+#dividing into train and test
+div = (floor(nrow(data)))*0.8
+train_data = data[1:div,]
+test_data = data[(div+1):nrow(data),]
+
+
+
+#checking stationarity of the data
+adf.test(train_data$Close)
+
+#taking first order difference
+y = diff(train_data$Close)
+
+#again checking stationarity of the data
+adf.test(y)
+
+#plotting the stationary data
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#finding the order of AR & MA using the acf and pacf
+acf(y)
+pacf(y)
+
+#checking order with auto.arima function
+auto.arima(train_data$Close)
+
+#fitting the arima model with p,d,q
+model_train = arima(train_data$Close, c(1,1,1))
+y.fit = forecast(model_train, length(test_data$Close))[[4]]
+
+#RMSE
+rmse(test_data$Close, y.fit)
+
+#plots
+ggplot()+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  geom_line(data = data, aes(x = 1 : nrow(data), y = Close, col = 'Original'))+
+  geom_line(data = test_data, aes(x = (div+1):nrow(data), y = y.fit, col = 'fitted'), size = 0.75)+
+  labs(x = 'Date',
+       y = 'Closing Price',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+#-----------------------------------------------------------------------------------
+#ARCH-GARCH
+
+#checking volatility clustering
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#checking whether to fit ARCH model or not
+ArchTest(y)
+
+y1 = diff(data$Close)
+
+#determining the order
+garch(x = y, grad = 'numerical', trace = FALSE)
+
+#fitting GARCH model
+y_garch = ugarchspec(variance.model = list(garchOrder = c(1,1)), mean.model=list(armaOrder=c(1,1)))
+
+y_garch_fit = ugarchfit(y_garch, data = y)
+
+y_garch_fit
+
+
+#forecasting
+y2 = diff(test_data$Close)
+y_forecast = (ugarchforecast(y_garch_fit, n.ahead = length(y2))@forecast)$series
+
+
+#plot(y_garch_fit)
+
+ggplot()+
+  geom_line(aes(x = 1 : length(y1), y = y1, col = 'Original'))+
+  geom_line(aes(x = (div+1):(nrow(data)-1), y = y_forecast, col = 'Fitted'), size = 0.75)+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  labs(x = 'Date',
+       y = 'Closing Price (stationary)',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+
+
+
+
+rmse(y2,y_forecast)
+#===================================================================================
+
+#5. TCS
+#========
+
+#data
+data = read.csv('TCS.NS.csv')
+
+
+#plotting the data against time
+ggplot(data)+
+  geom_line(aes(x = 1:nrow(data), y = Close))
+
+#dividing into train and test
+div = (floor(nrow(data)))*0.8
+train_data = data[1:div,]
+test_data = data[(div+1):nrow(data),]
+
+
+
+#checking stationarity of the data
+adf.test(train_data$Close)
+
+#taking first order difference
+y = diff(train_data$Close)
+
+#again checking stationarity of the data
+adf.test(y)
+
+#plotting the stationary data
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#finding the order of AR & MA using the acf and pacf
+acf(y)
+pacf(y)
+
+#checking order with auto.arima function
+auto.arima(train_data$Close)
+
+#fitting the arima model with p,d,q
+model_train = arima(train_data$Close, c(0,1,0))
+y.fit = forecast(model_train, length(test_data$Close))[[4]]
+
+#RMSE
+rmse(test_data$Close, y.fit)
+
+#plots
+ggplot()+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  geom_line(data = data, aes(x = 1 : nrow(data), y = Close, col = 'Original'))+
+  geom_line(data = test_data, aes(x = (div+1):nrow(data), y = y.fit, col = 'fitted'), size = 0.75)+
+  labs(x = 'Date',
+       y = 'Closing Price',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+#----------------------------------------------------------------------------------
+#ARCH-GARCH
+
+#checking volatility clustering
+ggplot()+
+  geom_line(aes(x = 1:length(y), y = y))
+
+#checking whether to fit ARCH model or not
+ArchTest(y)
+
+y1 = diff(data$Close)
+
+#determining the order
+garch(x = y, grad = 'numerical', trace = FALSE)
+
+#fitting GARCH model
+y_garch = ugarchspec(variance.model = list(garchOrder = c(1,1)), mean.model=list(armaOrder=c(0,0)))
+
+y_garch_fit = ugarchfit(y_garch, data = y)
+
+y_garch_fit
+
+
+#forecasting
+y2 = diff(test_data$Close)
+y_forecast = (ugarchforecast(y_garch_fit, n.ahead = length(y2))@forecast)$series
+
+
+#plot(y_garch_fit)
+
+ggplot()+
+  geom_line(aes(x = 1 : length(y1), y = y1, col = 'Original'))+
+  geom_line(aes(x = (div+1):(nrow(data)-1), y = y_forecast, col = 'Fitted'), size = 0.75)+
+  geom_vline(xintercept = div, linetype = 'dashed', col = 'navyblue', size = 0.6)+
+  labs(x = 'Date',
+       y = 'Closing Price (stationary)',
+       color = 'Index')+
+  scale_x_continuous(breaks = seq(1,nrow(data),500), labels = data$Date[seq(1,nrow(data),500)])+
+  theme(axis.text.x = element_text(angle = 10))
+
+
+
+
+rmse(y2,y_forecast)
